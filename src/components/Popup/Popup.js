7cc1deb56ec72e3eaba16c21c6260e6ce9a1551c
Italorcf <italo.rufca@gmail.com>
2019-09-27 15:00:39 -0300

    #1.3 PopUp

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Popup.css';
+
+const popup = (props) => {
+    return (
+        <div>
+            <div className="modal-wrapper"
+                style={{
+                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                <div className="modal-header">
+                    <h3>Bem Vindo a Amazônia Saqueada</h3>
+                    <span className="close-modal-btn" onClick={props.close}>×</span>
+                </div>
+                <div className="modal-body">
+                    <p>
+                        {props.children}
+                    </p>
+                </div>
+                <div className="modal-footer">
+                    <button className="btn-cancel" onClick={props.close}>Fechar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default popup;
